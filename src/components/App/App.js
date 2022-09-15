243dfe5587a6c1700636db3c0b118ccaac7d5c19
Rachel Bock <rachel.online@gmail.com>
2022-09-15 11:16:16 -0600

    Implement Search feature

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,34 +10,41 @@ class App extends Component {
     super();
     this.state = {
       headlines: [],
+      articles: [],
       selectedArticle: {}
     }
   }
+
+  handleSearch = (event) => {
+    let news = this.state.headlines.filter(story => story.title.includes(event.target.value));
+    this.setState({articles: [...news]});
+  };
   
   selectStory = (index) => {
     this.setState({selectedArticle: this.state.headlines[index]});
-  }
+  };
 
   componentDidMount() {
     getTopStories()
       .then(data => {
         console.log(data)
         this.setState({headlines: data.results});
+        this.setState({articles: [...this.state.headlines]});
       });
-  }
+  };
  
   render() {
     return (
       <div className='App'>
         <h1>NYTreats - A New York Times Top Stories Reader</h1>
-        <Navigation />
+        <Navigation handleSearch={this.handleSearch}/>
         <div className='body'>
-          <Listing headlines={this.state.headlines} selectStory={this.selectStory}/>
+          <Listing headlines={this.state.articles} selectStory={this.selectStory}/>
           {this.state.selectedArticle && <Main story={this.state.selectedArticle} />}
         </div>
       </div>
     );
-  }    
+  };    
 }
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,32 @@
+import React, { useState } from 'react';
 import './Navigation.css';
 
-const Navigation = () => {
-  return (
-    <div className="navigation">
-    </div>
-  );
+class Navigation extends component {
+  
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  };
+
+  handleChange = event => {
+    this.setState({search: event.target.value});
+  };
+ 
+  render() {
+    return (
+      <div className="navigation">
+        <input 
+          type='text' 
+          placeholder='Search...' 
+          name='search'
+          value={this.state.search}
+          onChange={event => handleSearch(event)}
+        />
+      </div>
+    );
+  }
 }
 
 export default Navigation;
